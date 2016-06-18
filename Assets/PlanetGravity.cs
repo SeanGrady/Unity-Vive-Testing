9e0be25a3149bab9b270d296b3cb6da0f87daa73
Sean <vedicmonk@gmail.com>
2016-06-18 13:30:53 -0700

    first commit

---
+++ Assets/PlanetGravity.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetGravity : MonoBehaviour {
+
+    GameObject moon;
+    GameObject planet;
+    public float gravConst;
+    public float planetMass;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        planet = gameObject;
+	}
+	
+	void FixedUpdate () {
+        if (moon != null)
+        {
+            var heading = planet.transform.position - moon.transform.position;
+            var distSquared = heading.sqrMagnitude;
+            var dist = heading.magnitude;
+            Debug.Log("distance is: ");
+            Debug.Log(dist);
+            var direction = heading / distSquared;
+            var forceNumerator = rb.mass * gravConst *planetMass;
+            var force = forceNumerator / distSquared;
+
+            rb.useGravity = false;
+            rb.AddForce(direction * force);
+        }
+	}
+
+    private void OnTriggerEnter(Collider collider) {
+        moon = collider.gameObject;
+        rb = moon.GetComponent<Rigidbody>();
+        // Debug.Log("planet trigger entered");
+    }
+
+    private void OnTriggerExit(Collider collider) {
+        rb.useGravity = true;
+        moon = null;
+        rb = null;
+    }
+}
---
+++ Assets/WandController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WandController : MonoBehaviour {
+
+    public Rigidbody attachPoint;
+    public float pullForce;
+
+    SteamVR_TrackedObject trackedObj;
+    FixedJoint joint;
+    GameObject pickup;
+
+    void Awake () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    // TODO: Something better than just GetTouch or GetTouchDown for grabbing object
+    //       (GetTouchDown with a bigger sphere maybe? who knows...)
+    void FixedUpdate ()
+    {
+        var device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (joint == null && device.GetTouch(SteamVR_Controller.ButtonMask.Trigger) && pickup == null)
+        {
+            Debug.Log("pulling on tiny cube?");
+            var magnetObj = GameObject.FindWithTag("Planetoid");
+            Debug.Log(magnetObj.name);
+            var mrb = magnetObj.GetComponent<Rigidbody>();
+            var heading = trackedObj.transform.position - mrb.transform.position;
+            var direction = heading / heading.magnitude;
+            mrb.useGravity = false;
+            mrb.AddForce(direction * pullForce);
+        }
+        if (joint == null && device.GetTouch(SteamVR_Controller.ButtonMask.Trigger) && pickup != null)
+        {
+            pickup.transform.position = attachPoint.transform.position;
+            joint = pickup.AddComponent<FixedJoint>();
+            joint.connectedBody = attachPoint;
+        }
+        else if (joint != null && device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            var rb = pickup.GetComponent<Rigidbody>();
+            Object.DestroyImmediate(joint);
+            rb.useGravity = true;
+            joint = null;
+
+			var origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
+			if (origin != null)
+			{
+				rb.velocity = origin.TransformVector(device.velocity);
+				rb.angularVelocity = origin.TransformVector(device.angularVelocity);
+			}
+			else
+			{
+				rb.velocity = device.velocity;
+				rb.angularVelocity = device.angularVelocity;
+			}
+
+			rb.maxAngularVelocity = rb.angularVelocity.magnitude;
+        }
+    }
+
+    void OnTriggerEnter (Collider collider)
+    {
+        pickup = collider.gameObject;
+        // Debug.Log("wand trigger entered");
+    }
+
+    void OnTriggerExit (Collider collider)
+    {
+        pickup = null;
+        // Debug.Log("wand trigger exited");
+    }
+}
